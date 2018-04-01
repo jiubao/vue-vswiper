4437a5f4f1e68be5b3f21846b2661d9e6e2eacd9
xiadong8 <xiadong8@wanda.cn>
2018-04-01 21:08:49 +0800

    gh-pages

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import swiper from './swiper.vue'
-import swiperItem from './swiper-item.vue'
-
-export default {
-  install (vue, options) {
-    vue.component('vswiper', swiper)
-    vue.component('vswiper-item', swiperItem)
-  }
-}
